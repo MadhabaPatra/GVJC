4fd50efe82fdf8d0d6c6c7a2364079a76677390c
G Madhabananda Patra <madhabapatra@gmail.com>
2019-11-21 16:55:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+
+
+<button onclick="myFunction()">Take me to w3schools.com</button>
+
+<script>
+function myFunction() {
+  location.href = "https://www.w3schools.com";
+}
+</script>
+
+</body>
+</html>
