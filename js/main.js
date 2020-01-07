5b5c6ed69e59f4da2d0c806e1210e3f7a4f4644c
unknown <madhabapatra@gmail.com>
2020-01-08 00:37:48 +0530

    animation and background activity

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*
+____________Menu-Toggle Mobile Version_______________
+*/
+
+$(document).ready(function(){
+	$('.menu-toggle').click(function(){
+	$('.menu-toggle').toggleClass('active');
+	$('nav').toggleClass('active');
+	})
+		});
+/*
+	 ____________Upload button_______________
+*/
+function udata(){
+    var a=document.getElementById("uploadBtn").value;
+    document.getElementById("uploadFile").value = a;
+}
+/*
+_____________Page Reload Resubmission Issue Solve_______________
+*/
+if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+}
+/*
+	 ____________Percentage Calculation_______________
+*/
+function markCalc(i) {
+    a=i.value;
+    p=a*100/600;
+    p=p.toFixed(2);
+    var x = document.getElementById("m");
+    r=p+"%";
+    x.value= r;
+  }
