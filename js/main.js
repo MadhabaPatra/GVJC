2c8f2a98df1372213108c4cf7f5056fab90d6b51
G Madhabananda Patra <madhabapatra@gmail.com>
2020-02-15 09:53:19 -0800

    Progressively load Content on scroll

--- js/main.js
+++ js/main.js
@@ -40,3 +40,22 @@ _____________ Carousel Time Interval______________
   $('.carousel').carousel({
     interval: 10
   })
+/*
+_____________ Progressively load Content on scroll______________
+*/
+jQuery(window).scroll(function(){
+
+    // get the bottom position
+    var bottom_position = $(document).height() - ($(window).scrollTop() + $(window).height());
+    var scroll_data = {
+            action: 'user_scroll',
+            container_id: container_id
+        };      
+    $.ajax({
+             url: wdm_url,
+             data: scroll_data,
+             context: document.body
+           }).done(function(response) {
+                // do stuff here
+              });     
+});
